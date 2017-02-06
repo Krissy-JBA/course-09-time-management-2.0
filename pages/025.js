7795da8fb99a362902e5ebb412b0fc585b70e38a
Alexandra Dunham <alexandra@prideandjoy.org>
2017-02-07 11:23:43 +1300

    update for mobile

--- pages/025.js
+++ pages/025.js
@@ -2,13 +2,32 @@ pageComponentry = {
   data: function() {
     return {
       // Any page specific data goes here.
-      button1: false,
-      button2: false,
-      button3: false
+      textOne: false,
+      textTwo: false,
+      textThree: false,
+      popup: false
     }
   },
   methods: {
     // Any page specific methods go here.
+    button2: function() {
+      this.popup = true;
+      this.textOne = true;
+      this.textTwo = false;
+      this.textThree = false;
+    },
+    button1: function() {
+      this.popup = true;
+      this.textOne = false;
+      this.textTwo = true;
+      this.textThree = false;
+    },
+    button3: function() {
+      this.popup = true;
+      this.textOne = false;
+      this.textTwo = false;
+      this.textThree = true;
+    }
   },
   ready: function() {
     // This will be fired when the page is loaded
